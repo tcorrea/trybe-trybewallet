68e77aed6ba5dee5795adfbdde272cc084e6a488
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-04-04 15:37:31 -0300

    feat: requisito 7

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -181,6 +181,25 @@ class Wallet extends React.Component {
             Adicionar despesa
           </button>
         </div>
+
+        <div>
+          <table>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+            <tr>
+              <td />
+            </tr>
+          </table>
+        </div>
       </>
     );
   }
